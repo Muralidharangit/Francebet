a9748486555536359fafb115f8a8d3616870f30a
Muralidharangit <125630876+Muralidharangit@users.noreply.github.com>
2025-09-30 15:13:38 +0530

    new changes voucher

--- src/Components/routes/route.js
+++ src/Components/routes/route.js
@@ -39,8 +39,8 @@ const routes = {
 
     // Kazang
     kazang_deposit_voucher: "/deposit-namibia-kazang",
-    kazang_deposit_history: "/deposit-namibia-kazang-history",
-    
+    kazang_deposit_history: "/Voucher-Deposit-History",
+
     // India
     manual_deposit_India: "/manual-deposit-india",
     manual_withdraw_India: "/manual-withdraw-india",
